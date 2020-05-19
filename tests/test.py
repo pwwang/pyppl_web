8d653bf713bb550b8b01b2457b97fd4519114cc7
pwwang <pwwang@pwwang.com>
2020-05-19 17:39:09 -0500

    Allow to import different pyppl for testing.

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent.resolve()))
 from pyppl import config_plugins, PyPPL, Proc, ProcSet
 from pyppl_web import PyPPLWeb
 
